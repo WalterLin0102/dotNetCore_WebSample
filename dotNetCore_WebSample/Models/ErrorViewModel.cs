a58e1223a41f1a86da22c614b30c81ecfbaf0ac5
walter <walterlin0102@bestcom.com.tw>
2021-05-17 17:17:43 +0800

    init

---
+++ dotNetCore_WebSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNetCore_WebSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
